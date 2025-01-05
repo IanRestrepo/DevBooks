6149ed9f7753dbba3de64c3d26909df23661e340
Ian Restrepo <ianrestrepog@gmail.com>
2025-01-05 15:01:34 -0500

    ADD | Server Islands and Content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
-
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +11,8 @@ export default defineConfig({
 
   env: {
     schema: {
-      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access:'public' }),
-      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'})
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
     }
   },
 
